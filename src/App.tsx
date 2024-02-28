a5829569ad4c012e95caa9ca671922bbb5977e9d
tomcorey26 <tomcorey26@my.uri.edu>
2024-02-28 15:03:44 -0500

    Can add cards to the list

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
-import { SentenceAdder } from '@/components/custom/SentenceAdder';
 import './App.css';
+import { Input } from '@/components/ui/input';
+import { Card } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
 
 // Features:
 // 1) Form to add a new line item. English and spanish translation
@@ -11,12 +13,53 @@ import './App.css';
 // Strech goals:
 // Have it translate to other languages automatically
 
+interface FlashCard {
+  english: string;
+  translation: string;
+}
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [flashCards, setFlashCards] = useState<FlashCard[]>([
+    { english: 'hello', translation: 'hola' },
+  ]);
+  const [english, setEnglish] = useState('');
+  const [translation, setTranslation] = useState('');
+
+  function addFlashCard() {
+    setFlashCards([
+      {
+        english,
+        translation,
+      },
+      ...flashCards,
+    ]);
+    setEnglish('');
+    setTranslation('');
+  }
 
   return (
     <div>
-      <SentenceAdder />
+      <div className="grid grid-cols-2 gap-4">
+        <Input
+          placeholder="English"
+          value={english}
+          onChange={(e) => setEnglish(e.target.value)}
+        />
+        <Input
+          placeholder="Translation"
+          value={translation}
+          onChange={(e) => setTranslation(e.target.value)}
+        />
+        <Button onClick={addFlashCard} className="col-span-2">
+          Add
+        </Button>
+        {flashCards.map((flashCard) => (
+          <>
+            <Card>{flashCard.english}</Card>
+            <Card>{flashCard.translation}</Card>
+          </>
+        ))}
+      </div>
     </div>
   );
 }
