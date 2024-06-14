38904464b0ad6ed48422b9ec126d4b319b0a8671
tomcorey26 <tomcorey26@my.uri.edu>
2024-06-14 13:17:11 -0400

    Mexico voice

--- src/services/TextToSpeechService.ts
+++ src/services/TextToSpeechService.ts
@@ -2,7 +2,8 @@ export function speakSpanish(text: string) {
   const msg = new SpeechSynthesisUtterance(text);
   const voices = window.speechSynthesis.getVoices();
   msg.voice = voices.filter(function (voice) {
-    return voice.lang == 'es-ES';
+    // get mexican spanish voice
+    return voice.lang === 'es-MX';
   })[0];
   window.speechSynthesis.speak(msg);
 }
